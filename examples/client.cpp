8a2bc01951279f439c7375c6c2807689ce6c75ea
sehe <sgheeren@gmail.com>
2021-02-10 00:24:20 +0100

    fixup cmake & examples for modernized version

--- examples/client.cpp
+++ examples/client.cpp
@@ -1,16 +1,45 @@
 #include <msghub.h>
+#include <boost/date_time.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/container/flat_map.hpp>
+#include <boost/container/static_vector.hpp>
 #include <iostream>
+#include <system_error>
+#include <thread>
+using namespace std::chrono_literals;
+using boost::filesystem::last_write_time;
+using boost::posix_time::from_time_t;
 
-int main() {
-    boost::asio::io_service io_service;
-    msghub msghub(io_service);
-    if (!msghub.connect("localhost", 1334)) {
-        std::cerr << "Unable to connect to hub\n";
-    } else {
-        if (!msghub.publish("Publish", "new message")) {
-            std::cerr << "Unable to post message on Publish channel\n";
+namespace mh = msghublib;
+int main(int /*argc*/, char const** argv) {
+    static auto const bin_date = to_simple_string(from_time_t(last_write_time(argv[0])));
+
+    boost::asio::thread_pool io(10);
+    mh::msghub hub(io.get_executor());
+
+    static std::atomic_int batches = 0;
+    try {
+        hub.connect("localhost", 1334);
+
+        hub.publish("Publish", bin_date);
+        for (auto i = 0; i < 1'000; ++i) {
+            ++batches;
+            post(io, [&hub, n = i * 100] {
+                mh::error_code ec;
+                for (int i = n; i < n + 100; ++i) {
+                    hub.publish("Publish", "message " + std::to_string(i), ec);
+                    if (ec) {
+                        std::cerr << "Couldn't publish message " << i << " (" << ec.message() << ")\n";
+                    }
+                }
+                --batches;
+            });
         }
+    } catch (mh::system_error const& se) {
+        std::cerr << "Error " << se.code().message() << "\n";
     }
 
-    //msghub.join(); // implied in destructor
+    while (batches > 0) { std::this_thread::sleep_for(10ms); }
+    hub.stop();
+    io.join();
 }
--- examples/server.cpp
+++ examples/server.cpp
@@ -1,28 +1,80 @@
 #include <boost/asio.hpp>
+#include <boost/asio/signal_set.hpp>
 #include <msghub.h>
 #include <iostream>
 #include <iomanip>
+#include <atomic>
+#include <boost/histogram.hpp>
+#include <boost/spirit/home/x3.hpp>
+#include <mutex>
+#include <thread>
+namespace mh = msghublib;
+namespace bh = boost::histogram;
+namespace x3 = boost::spirit::x3;
 
-void on_message(const std::string& topic, std::vector<char> const& message)
+namespace {
+    constexpr auto io_threads = 10;
+    std::mutex mx;
+    auto h = bh::make_profile(bh::axis::integer<>(0, io_threads) );
+} // namespace
+
+static void on_message(std::string_view topic, mh::span<char const> message)
 {
-    std::cout << "on_message: " << std::quoted(topic) << " " <<
-        std::quoted(std::string(message.begin(), message.end())) << std::endl;
+    size_t value = 0;
+    x3::parse(message.begin(), message.end(), x3::seek[x3::uint_], value);
+
+    static std::atomic_int counter(0);
+    if (0 == (counter++ % 1'000)) {
+        std::string_view sm(message.data(), message.size());
+        std::cout << (counter-1) << " callback: " << topic << " " << sm << "\n";
+    }
+
+    {
+        static std::atomic_int tid_gen(0);
+        thread_local const int tid = tid_gen++;
+
+        std::lock_guard lk(mx);
+        h(tid, bh::sample(value));
+    }
+
+    //if (counter>9'000) throw "TOO DAMN HIGH";
 }
 
 int main()
 {
-    boost::asio::io_service io_service;
-    boost::asio::io_service::work  work(io_service);
-    // Create hub to listen on 0xbee port
-    msghub msghub(io_service);
-    if (!msghub.create(1334))
-        std::cerr << "Error creating hub server\n";
-    // Subscribe on "any topic"
-    if (!msghub.subscribe("Publish", on_message))
-        std::cerr << "Error creating subscription\n";
+    try {
+        boost::asio::thread_pool io(io_threads);
+        // Create hub to listen on 0xbee port
+        mh::msghub hub(io.get_executor());
+        hub.create(1334);
+        // Subscribe on "any topic"
+        hub.subscribe("Publish", on_message);
+        // Current or any another client
+        //hub.publish("Publish", "hello");
 
-    // Current or any another client
-    //msghub.publish("Publish", "hello");
+        boost::asio::signal_set ss(io, SIGINT);
+        ss.async_wait([&](auto ec, int sig) {
+            if (!ec) {
+                std::cerr << "Shutdown requested (" << ::strsignal(sig) << ", " << ec.message() << ")\n";
+                hub.stop();
+            }
+        });
 
-    io_service.run(); // blocks for-ever
+        io.join(); // blocks
+    } catch(std::exception const& e) {
+        std::cerr << "Exception " << e.what() << "\n";
+    } catch(...) {
+        std::cerr << "Exception\n";
+    }
+
+    for (auto&& x : indexed(h)) {
+        if (x->count() != 0.0) { 
+            std::cout
+                << "thread " << x.index()
+                << "\tcount " << x->count()
+                << "\tmean " << x->value()
+                << "\tstddev " << std::sqrt(x->variance())
+                << "\n";
+        }
+    }
 }
